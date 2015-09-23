ee022ebabb494cb83a7cc6b131f1980a4a634170
rharriso <rtharrison86@gmail.com>
2015-09-22 23:38:44 -0400

    fix audio fail

--- www/js/audio-service.js
+++ www/js/audio-service.js
@@ -1,85 +1,102 @@
-angular.module('tone-timer.services', ['ionic'])
+(function(){
+  'use strict';
 
-// for media plugin : http://plugins.cordova.io/#/package/org.apache.cordova.media
-.factory('AudioService', function($q, $ionicPlatform, $window){
-  var service = {
-    loadMedia: loadMedia,
-    playMedia: playMedia,
-    getStatusMessage: getStatusMessage,
-    getErrorMessage: getErrorMessage
-  };
-
-  function playMedia(src){
-    loadMedia(src).then(function(media){
-      media.play();
-    });
-  }
-
-  function loadMedia(src, onStop, onError, onStatus){
-    var defer = $q.defer();
-    $ionicPlatform.ready(function(){
-      var mediaStatus = {
-        code: 0,
-        text: getStatusMessage(0)
-      };
-      var mediaSuccess = function(){
-        mediaStatus.code = 4;
-        mediaStatus.text = getStatusMessage(4);
-        if(onStop){onStop();}
-      };
-      var mediaError = function(err){
-        _logError(src, err);
-        if(onError){onError(err);}
-      };
-      var mediaStatus = function(status){
-        mediaStatus.code = status;
-        mediaStatus.text = getStatusMessage(status);
-        if(onStatus){onStatus(status);}
-      };
+  var app = angular.module('tone-timer.services', ['ionic']);
+  var audioHash = {};
 
 
-      // create and play with audio tag
-      if($ionicPlatform.is('browser')){ 
-        $("audio, video").remove();
-        var media = document.createElement("audio");
-        document.body.appendChild(media);
-        media.src = src;
-        defer.resolve(media);
-        return defer
-      }
+  // for media plugin : http://plugins.cordova.io/#/package/org.apache.cordova.media
+  app.factory('AudioService', function($q, $ionicPlatform, $window){
+    var service = {
+      loadMedia: loadMedia,
+      playMedia: playMedia,
+      getStatusMessage: getStatusMessage,
+      getErrorMessage: getErrorMessage
+    };
 
-      // play through cordova
-      if($ionicPlatform.is('android')){src = '/android_asset/www/' + src;}
-      var media = new $window.Media(src, mediaSuccess, mediaError, mediaStatus);
-      media.status = mediaStatus;
-      defer.resolve(media);
-    });
-    return defer.promise;
-  }
+    function playMedia(src){
+      loadMedia(src).then(function(media){
+        // go to beginning
+        if(media.seekTo){
+          media.seekTo(0);
+        } else {
+          media.currentTime = 0;
+        }
+        // play
+        media.play();
+      });
+    }
 
-  function _logError(src, err){
-    console.error('MediaSrv error', {
-      code: err.code,
-      text: getErrorMessage(err.code)
-    });
-  }
+    function loadMedia(src, onStop, onError, onStatus){
+      var defer = $q.defer();
+      $ionicPlatform.ready(function(){
+        var mediaStatus = {
+          code: 0,
+          text: getStatusMessage(0)
+        };
+        var mediaSuccess = function(){
+          mediaStatus.code = 4;
+          mediaStatus.text = getStatusMessage(4);
+          if(onStop){onStop();}
+        };
+        var mediaError = function(err){
+          _logError(src, err);
+          if(onError){onError(err);}
+        };
+        var mediaStatus = function(status){
+          mediaStatus.code = status;
+          mediaStatus.text = getStatusMessage(status);
+          if(onStatus){onStatus(status);}
+        };
 
-  function getStatusMessage(status){
-    if(status === 0){return 'Media.MEDIA_NONE';}
-    else if(status === 1){return 'Media.MEDIA_STARTING';}
-    else if(status === 2){return 'Media.MEDIA_RUNNING';}
-    else if(status === 3){return 'Media.MEDIA_PAUSED';}
-    else if(status === 4){return 'Media.MEDIA_STOPPED';}
-    else {return 'Unknown status <'+status+'>';}
-  }
 
-  function getErrorMessage(code){
-    if(code === 1){return 'MediaError.MEDIA_ERR_ABORTED';}
-    else if(code === 2){return 'MediaError.MEDIA_ERR_NETWORK';}
-    else if(code === 3){return 'MediaError.MEDIA_ERR_DECODE';}
-    else if(code === 4){return 'MediaError.MEDIA_ERR_NONE_SUPPORTED';}
-    else {return 'Unknown code <'+code+'>';}
-  }
+        // create and play with audio tag
+        if($ionicPlatform.is('browser')){ 
+          $("audio, video").remove();
+          var media = document.createElement("audio");
+          document.body.appendChild(media);
+          media.src = src;
+          defer.resolve(media);
 
-  return service;
-});
+        // play through cordova
+        } else{
+          if($ionicPlatform.is('android')){src = '/android_asset/www/' + src;}
+          if(!audioHash[src]){
+            audioHash[src] = new $window.Media(src, mediaSuccess, mediaError, mediaStatus);
+          }
+
+          audioHash[src].status = mediaStatus;
+          defer.resolve(audioHash[src]);
+        }
+      });
+      return defer.promise;
+    }
+
+    function _logError(src, err){
+      console.error('MediaSrv error', {
+        code: err.code,
+        text: getErrorMessage(err.code)
+      });
+    }
+
+    function getStatusMessage(status){
+      if(status === 0){return 'Media.MEDIA_NONE';}
+      else if(status === 1){return 'Media.MEDIA_STARTING';}
+      else if(status === 2){return 'Media.MEDIA_RUNNING';}
+      else if(status === 3){return 'Media.MEDIA_PAUSED';}
+      else if(status === 4){return 'Media.MEDIA_STOPPED';}
+      else {return 'Unknown status <'+status+'>';}
+    }
+
+    function getErrorMessage(code){
+      if(code === 1){return 'MediaError.MEDIA_ERR_ABORTED';}
+      else if(code === 2){return 'MediaError.MEDIA_ERR_NETWORK';}
+      else if(code === 3){return 'MediaError.MEDIA_ERR_DECODE';}
+      else if(code === 4){return 'MediaError.MEDIA_ERR_NONE_SUPPORTED';}
+      else {return 'Unknown code <'+code+'>';}
+    }
+
+    return service;
+  });
+})();
+
